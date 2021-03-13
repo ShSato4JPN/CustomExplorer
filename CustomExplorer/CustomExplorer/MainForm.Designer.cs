2f67d740a635053b6dc42359a53bb32479f00628
ShSato4_Note <48849803+ShSato4JPN@users.noreply.github.com>
2021-03-13 14:03:04 +0900

    新規ファイルをリポジトリに追加

---
+++ CustomExplorer/CustomExplorer/MainForm.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace CustomExplorer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.SubButton = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.ExplorerList = new System.Windows.Forms.DataGridView();
+            this.ExpImageCol = new System.Windows.Forms.DataGridViewImageColumn();
+            this.ExpNameCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ExpTypeCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.ExplorerList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SubButton
+            // 
+            this.SubButton.Location = new System.Drawing.Point(226, 12);
+            this.SubButton.Name = "SubButton";
+            this.SubButton.Size = new System.Drawing.Size(40, 40);
+            this.SubButton.TabIndex = 1;
+            this.SubButton.Text = "ー";
+            this.SubButton.UseVisualStyleBackColor = true;
+            this.SubButton.Click += new System.EventHandler(this.SubButton_Click);
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(272, 12);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(40, 40);
+            this.AddButton.TabIndex = 2;
+            this.AddButton.Text = "＋";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // ExplorerList
+            // 
+            this.ExplorerList.AllowUserToAddRows = false;
+            this.ExplorerList.AllowUserToResizeColumns = false;
+            this.ExplorerList.AllowUserToResizeRows = false;
+            this.ExplorerList.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.ExplorerList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ExplorerList.ColumnHeadersVisible = false;
+            this.ExplorerList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ExpImageCol,
+            this.ExpNameCol,
+            this.ExpTypeCol});
+            this.ExplorerList.Location = new System.Drawing.Point(12, 58);
+            this.ExplorerList.MultiSelect = false;
+            this.ExplorerList.Name = "ExplorerList";
+            this.ExplorerList.RowHeadersVisible = false;
+            this.ExplorerList.RowHeadersWidth = 51;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle1.Padding = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.ExplorerList.RowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.ExplorerList.RowTemplate.Height = 20;
+            this.ExplorerList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.ExplorerList.Size = new System.Drawing.Size(300, 733);
+            this.ExplorerList.TabIndex = 4;
+            this.ExplorerList.TabStop = false;
+            this.ExplorerList.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ExplorerList_CellClick);
+            this.ExplorerList.RowPrePaint += new System.Windows.Forms.DataGridViewRowPrePaintEventHandler(this.ExplorerList_RowPrePaint);
+            this.ExplorerList.SelectionChanged += new System.EventHandler(this.ExplorerList_SelectionChanged);
+            this.ExplorerList.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ExplorerList_KeyDown);
+            // 
+            // ExpImageCol
+            // 
+            this.ExpImageCol.HeaderText = "image";
+            this.ExpImageCol.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Zoom;
+            this.ExpImageCol.MinimumWidth = 50;
+            this.ExpImageCol.Name = "ExpImageCol";
+            this.ExpImageCol.ReadOnly = true;
+            this.ExpImageCol.Width = 50;
+            // 
+            // ExpNameCol
+            // 
+            this.ExpNameCol.HeaderText = "name";
+            this.ExpNameCol.MinimumWidth = 6;
+            this.ExpNameCol.Name = "ExpNameCol";
+            this.ExpNameCol.ReadOnly = true;
+            this.ExpNameCol.Width = 250;
+            // 
+            // ExpTypeCol
+            // 
+            this.ExpTypeCol.HeaderText = "type";
+            this.ExpTypeCol.MinimumWidth = 6;
+            this.ExpTypeCol.Name = "ExpTypeCol";
+            this.ExpTypeCol.ReadOnly = true;
+            this.ExpTypeCol.Visible = false;
+            this.ExpTypeCol.Width = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1282, 803);
+            this.Controls.Add(this.ExplorerList);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.SubButton);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ExplorerList)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button SubButton;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.DataGridView ExplorerList;
+        private System.Windows.Forms.DataGridViewImageColumn ExpImageCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ExpNameCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ExpTypeCol;
+    }
+}
+
---
+++ CustomExplorer/CustomExplorer/MainForm.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomExplorer
+{
+    public enum  RowType
+    {
+        NORMAL = 0,
+        FAVORITE = 1
+    }
+    public partial class MainForm : Form
+    {
+        private int prevRowNo = 0;
+        private int prevColNo = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            ExplorerList.RowTemplate.Height = 70;
+            ExplorerList.Rows.Add(Image.FromFile(@"image\favorite.png"), "お気に入り", RowType.FAVORITE);
+            prevRowNo = ExplorerList.CurrentRow.Index;
+        }
+
+        /// <summary>
+        /// エクスプローラーリストにデータを追加する
+        /// </summary>
+        /// <param name="sender">自動生成</param>
+        /// <param name="e">自動生成</param>
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            Object[] insData = new Object[] { Image.FromFile(@"image\folder_close.png"), "テストデータ1です", RowType.NORMAL};
+            ExplorerList.Rows.Insert(0, insData);
+        }
+
+        /// <summary>
+        /// 選択されているデータをエクスプローラーリストから削除する
+        /// </summary>
+        /// <param name="sender">自動生成</param>
+        /// <param name="e">自動生成</param>
+        private void SubButton_Click(object sender, EventArgs e)
+        {
+            if ( ExplorerList.CurrentRow.Cells[2].Value.Equals(RowType.NORMAL) )
+            {
+                ExplorerList.Rows.Remove(ExplorerList.CurrentRow);
+            } 
+        }
+
+        /// <summary>
+        /// キー押下処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ExplorerList_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (ExplorerList.CurrentRow.Cells[2].Value.Equals(RowType.NORMAL))
+            {
+                switch (e.KeyCode)
+                {
+                    // F2キー
+                    case Keys.F2:
+                        ExplorerList.CurrentRow.Cells[1].ReadOnly = false;
+                        ExplorerList.CurrentRow.Cells[1].Selected = true;
+                        ExplorerList.BeginEdit(true);
+                        break;
+
+                }
+            }
+            prevRowNo = ExplorerList.CurrentRow.Index;
+        }
+
+        /// <summary>
+        /// 選択項目更処理
+        /// </summary>
+        /// <param name="sender">自動生成</param>
+        /// <param name="e">自動生成</param>
+        private void ExplorerList_SelectionChanged(object sender, EventArgs e)
+        {
+            // 全行で変更した値を初期化する
+            ExplorerList.CurrentRow.Cells[1].ReadOnly = true;
+
+            // 直前に参照していた行が Normal の時、画像を変更する
+            if (ExplorerList.Rows[prevRowNo].Cells[2].Value.Equals(RowType.NORMAL))
+            {
+                ExplorerList.Rows[prevRowNo].Cells[0].Value = Image.FromFile(@"image\folder_close.png");
+            }
+
+            // 参照行が Normal の時、画像を変更する
+            if (ExplorerList.CurrentRow.Cells[2].Value.Equals(RowType.NORMAL))
+            {
+                ExplorerList.CurrentRow.Cells[0].Value = Image.FromFile(@"image\folder_open.png");
+            }
+            prevRowNo = ExplorerList.CurrentRow.Index;
+        }
+
+        /// <summary>
+        /// セルクリック処理
+        /// </summary>
+        /// <param name="sender">自動生成</param>
+        /// <param name="e">自動生成</param>
+        private void ExplorerList_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// フォーカス時の枠線を非表示化する
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ExplorerList_RowPrePaint(object sender, DataGridViewRowPrePaintEventArgs e)
+        {
+            //フォーカス枠を描画しない
+            e.PaintParts &= ~DataGridViewPaintParts.Focus;
+        }
+    }
+}
